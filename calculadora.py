4186f4aa61c2d203d13723c6d569a7fe8b66b853
Davi Rocha <davi.rcardoso04@outlook.com>
2026-09-11 19:14:40 -0300

    add funcao de subtração

--- calculadora.py
+++ calculadora.py
@@ -6,4 +6,10 @@ def adicao(num1, num2):
 
 print("RESULTADO: ", adicao(16, 9))
 
+def subtracao(num1, num2):
+    subtracao = num1 - num2
+    return subtracao
+
+print("RESULTADO:" , subtracao (7,9))
+
     
