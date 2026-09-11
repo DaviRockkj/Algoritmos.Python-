8b283126c59fdfe2a45feecddf7f7b79965e22f3
Davi Rocha <davi.rcardoso04@outlook.com>
2026-09-11 19:20:28 -0300

    ADD FUNCAO DE MULTI, SUB E DIV

--- calculadora.py
+++ calculadora.py
@@ -12,4 +12,14 @@ def subtracao(num1, num2):
 
 print("RESULTADO:" , subtracao (7,9))
 
-    
+def multiplicacao(num1, num2):
+    multiplicacao = num1 * num2
+    return multiplicacao
+
+print("RESULTADO:" , multiplicacao (10,20))
+
+def divisao(num1, num2):
+    divisao = num1 / num2
+    return divisao
+
+print("RESULTADO:" , divisao (100,20))
